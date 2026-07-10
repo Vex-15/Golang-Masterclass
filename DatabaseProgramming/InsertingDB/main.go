d41a0b49200769186ba91b16b4be4f2fa8ef42d2
Vedant Kowdiki <vkowdiki@gmail.com>
2026-07-10 09:31:52 +0530

    Added 'InsertingDB' operations

--- DatabaseProgramming/InsertingDB/main.go
+++ DatabaseProgramming/InsertingDB/main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
 	_ "github.com/lib/pq" // To register the driver.
 )
@@ -19,7 +18,6 @@ CREATE TABLE IF NOT EXISTS users (
 
 func main() {
 	dsn := "host=localhost port=5432 user=postgres password=admin dbname=go_db sslmode=disable"
-	_ = os.Remove(dsn)
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
@@ -30,7 +28,7 @@ func main() {
 		if err := db.Close(); err != nil {
 			log.Printf("Error closing the database:")
 		}
-	}()
+	}() // can be replaced with db.Close() but this is a better way to handle errors
 
 	err = db.Ping()
 	if err != nil {
@@ -38,9 +36,35 @@ func main() {
 	}
 	fmt.Println("Connected to the database successfully!")
 
-	res, err := db.Exec(schema)
+	//createTable(db)
+	//commented because we already created the table in the previous example
+
+	lastID, err := insertUser(db, "vex", "vex-15@gmail.com")
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Last user ID is ", lastID)
+}
+
+func createTable(db *sql.DB) {
+	res, err := db.Exec(schema)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Schema created successfully!", res)
+}
+
+func insertUser(db *sql.DB, name string, email string) (int, error) {
+	var id int
+	stmt := `INSERT INTO users(name, email) VALUES($1,$2) RETURNING id`
+
+	err := db.QueryRow(stmt, name, email).Scan(&id)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
